6eb77f45b3110f167d8a1abb3c6511b7676ce2a4
MehmeTTEmre <18290056@ogrenci.ankara.edu.tr>
2022-03-04 21:18:00 +0300

    update

--- form/admin.py
+++ form/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Form
-from django_admin_listfilter_dropdown.filters import DropdownFilter, RelatedDropdownFilter, ChoiceDropdownFilter
+from django_admin_listfilter_dropdown.filters import DropdownFilter
 
 # Register your models here.
 
